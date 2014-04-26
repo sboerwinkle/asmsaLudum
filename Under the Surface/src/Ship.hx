f82708354542f46933eecdfb13079edbdbaceec6
Simon <sboerwinkle@gmail.com>
2014-04-25 21:29:08 -0500

    Added ship physics

--- Under the Surface/src/Ship.hx
+++ Under the Surface/src/Ship.hx
@@ -10,11 +10,17 @@ class Ship extends Sprite
 	var sprite:Sprite;
 	var sailAngle:Float;
 	var rudderAngle:Float;
+	var speed:Float;
 	public function new() 
 	{
 		sprite = new Sprite();
 		sprite.rotation = 90;
 		super();
 	}
-	
+	public function act(windAngle:Float, windSpeed:Float):Void {
+		var force = windSpeed * Math.cos(sprite.rotation + sailAngle-windAngle);
+		speed += force * Math.cos(sailAngle);
+		x += speed * Main.cos(sprite.rotation);
+		y += speed * Main.sin(sprite.rotation);
+	}
 }
